d0c258e9dd4f6219d4a481d3ddc0a9bb69fe5e7c
Роман <i4pion@yandex.ru>
2024-09-25 16:28:32 +0300

    Передача id на сервер после получения видео на клиент

--- guitar_shop/api/urls.py
+++ guitar_shop/api/urls.py
@@ -3,12 +3,14 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import video_process
 
 urlpatterns = [
     path('users/', views.UserList.as_view()),
     path('users/<int:pk>/', views.UserDetail.as_view()),
     path('guitar/', views.GuitarList.as_view()),
     path('upload/', views.VideoUploadView.as_view()),
+    path('process/', video_process, name="video_process"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 #+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 #urlpatterns = format_suffix_patterns(urlpatterns)
--- guitar_shop/api/views.py
+++ guitar_shop/api/views.py
@@ -4,7 +4,11 @@ from rest_framework.response import Response
 from rest_framework import status
 from . import serializers
 from django.contrib.auth.models import User
-from .models import Guitars
+from .models import Guitars, Video
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.decorators import api_view
+import sys
+
 
 
 
@@ -31,6 +35,14 @@ class VideoUploadView(generics.CreateAPIView):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         video = serializer.save()
-        # Получаем полный путь к файлу
         file_url = request.build_absolute_uri(video.file.url)
-        return Response({'file_url': file_url}, status=status.HTTP_201_CREATED)
+        file_id = Video.objects.get(file=video.file)
+        return Response({'file_url': file_url, 'file_id':file_id.id}, status=status.HTTP_201_CREATED)
+    
+@api_view(['POST'])
+def video_process(request):
+    number = request.data.get('id')
+    print(number)
+    if isinstance(number, (int, float)):
+        return Response({'message': 'Number received!', 'number': number}, status=status.HTTP_200_OK)
+    return Response({'error': 'Invalid input'}, status=status.HTTP_400_BAD_REQUEST)
